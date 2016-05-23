416bbbe2425d0de5a6d3bb040abee5f336495401
Dimitri Vaughn <dimitrivaughn@Zipcoder27s-MacBook-Pro.local>
2016-05-23 17:09:19 -0400

    added UML

---
+++ src/main/java/driverslicense/MyStringUtils.java
@@ -0,0 +1,13 @@
+package driverslicense;
+
+/**
+ * Created by dimitrivaughn on 5/23/16.
+ */
+public class MyStringUtils {
+
+    public String combineString(String[] array){
+        return null;
+    }
+
+
+}
---
+++ src/test/java/driverslicense/MyStringUtilsSpec.java
@@ -0,0 +1,21 @@
+package driverslicense;
+import org.junit.Test;
+
+import java.io.*;
+
+
+/**
+ * Created by dimitrivaughn on 5/23/16.
+ */
+public class MyStringUtilsSpec {
+
+    String[] stringCombine = {"a", "bow", "wow", "low", "key"};
+
+    @Test
+    public void stringCombineTest(){
+
+
+    }
+
+
+}
